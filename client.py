51a60bae18c61381dac68b7d3b9993231db58016
QuyeN1104 <165477449+QuyeN1104@users.noreply.github.com>
2024-12-03 19:36:24 +0700

    3/12/2024 cloud coffee

--- client.py
+++ client.py
@@ -5,35 +5,124 @@ import os
 import math
 import concurrent.futures
 import sys
-
-PORT = 5051
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDRESS =(SERVER, PORT)
+import threading
+from concurrent.futures import ThreadPoolExecutor
+LENGTH_NUMBER_OF_FILE = 10
+LENGTH_NAME = 1024
+ENCODING = 'utf-8'
 LENGTH_SIZE = 16 #16 bytes để truyền kích thước file
 LENGTH_MODE = 8  # 8 bytes để đọc mode
-LENGTH_MESS = 13 # 13 bytes tín hiệu phản hồi lại bên gửi
-BUFFER = 1024   # bộ nhớ đệm 1024 bytes
+LENGTH_MESS = 16 # 13 bytes tín hiệu phản hồi lại bên gửi
+SIZE = 16
+PORT_SERVER = 9999
+HOST_SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS_SERVER = (HOST_SERVER, PORT_SERVER)
+BUFFER = 1024
 message_notenough = 'NOTENOUGH'
 message_enough = 'ENOUGH'
 message_success = 'SUCCESS'
+message_failure = 'FAILURE'
 message_error_notfound = 'ERRORNOTFOUND'
+stop_signal = "NOTENOUGH"
+message_setup_first_pass_word = 'SETUP_pass_word'
+message_setup_first_pin = 'SETUP_PIN'
 
-def init_client():
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect(ADDRESS)
-    return client
+def add_padding(data, sum_byte):
+    if len(data) == sum_byte:
+        return data
+    return data + ' ' * (sum_byte - len(data))
+
+def enter_password(client):
+    message = client.recv(LENGTH_MESS).decode().strip()
+    print(message)
+
+    if message == message_setup_first_pass_word:
+        initpassword = input('Khoi tao password: ')
+        client.send(initpassword.ljust(LENGTH_SIZE).encode(ENCODING))  # Đảm bảo mật khẩu có đủ độ dài
+    else:
+        password = input('Nhap vao pass word: ')
+        client.send(password.ljust(LENGTH_SIZE).encode(ENCODING))  # Đảm bảo mật khẩu có đủ độ dài
+
+def enter_pin(client):
+    message = client.recv(LENGTH_MESS).decode().strip()
+    print(message)
+
+    if message == message_setup_first_pin:
+        initpin = input('Khoi tao pin: ')
+        client.send(initpin.ljust(LENGTH_SIZE).encode(ENCODING))  # Đảm bảo pin có đủ độ dài
+    else:
+        pin = input('Nhap vao pin: ')
+        client.send(pin.ljust(LENGTH_SIZE).encode(ENCODING))  # Đảm bảo pin có đủ độ dài
+
+def process_login_upload(client): #xử lí đăng nhập
+     # Gọi hàm nhập mật khẩu
+            enter_pin(client)
+
+            # Nhận phản hồi từ server
+            message = client.recv(LENGTH_MESS).decode().strip()
+            print(message)
+
+            while message == message_failure:
+                print('SAI PIN')
+                enter_pin(client)
+                message = client.recv(LENGTH_MESS).decode().strip()
+                print('message', message)
+                # return None
+
+def process_login_client(client): #xử lí đăng nhập
+     # Gọi hàm nhập mật khẩu
+            enter_password(client)
+
+            # Nhận phản hồi từ server
+            message = client.recv(LENGTH_MESS).decode().strip()
+            print(message)
+
+            while message == message_failure:
+                print('SAI PASS WORD')
+                enter_password(client)
+                message = client.recv(LENGTH_MESS).decode().strip()
+                # return None
+
+
+def init():
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDRESS_SERVER)
+            # process_login_client(client)
+            return client  # Mật khẩu đúng, kết nối thành công
+        except Exception as e:
+            print(f"Error: {e}")
+
+#xử lí tên đường dẫn để lấy tên file
+def cut_name_in_path(name_file):
+    positive_last_slash = 0
+    for i in range(len(name_file) - 1,-1,-1):
+        if name_file[i] == '/' or name_file[i] == "\\":
+            positive_last_slash = i
+            break
+    
+    if positive_last_slash == 0:
+        return name_file
+    name_result = name_file[positive_last_slash+1:len(name_file)]
+    return name_result
 
 #xử lý lấy tổng dung lượng file và gửi dung lượng file
-def send_path_and_size(client, path_file): 
-    file_size = os.path.getsize(path_file)
-    file_size_string = str(file_size).ljust(LENGTH_SIZE)
-    client.send(path_file.ljust(BUFFER).encode())
-    client.send(file_size_string.encode())
+def send_header_to_server(client, name_file,file):
+    file_size = os.path.getsize(name_file)
+    file_size_string = str(file_size)
+    name_file_nopath = cut_name_in_path(name_file)
+    client.send(name_file_nopath.ljust(LENGTH_NAME,' ').encode(ENCODING))
+    client.send(file_size_string.ljust(LENGTH_SIZE,' ').encode(ENCODING))
     return file_size
 
-# viết hàm một chức năng thôi 
-def send_content_to_server(client, name_file, file_size):
+#nhận dữ liệu từ server
+def client_receive_signal(client):
+    return client.recv(9)
+
+#Gửi các dữ liệu qua server
+def send_data_to_upload(name_file,client):
     file = open(name_file,"rb")
+    file_size = send_header_to_server(client, name_file,file)
     while True:
         #tạo thanh % t
         numberOfRepeat =math.ceil(file_size/ BUFFER)
@@ -46,69 +135,118 @@ def send_content_to_server(client, name_file, file_size):
                 time.sleep(0.0000000005) 
                 bar()
             break
-    received_message = client.recv(LENGTH_MESS).decode().strip()
-    if received_message == message_notenough :
-        send_content_to_server(client, name_file, file_size)
-    else: print('Đã gửi thành công.')
+    file.close()
+#Upload file
+def upload(client):
+    process_login_upload(client)
+    name_file = input("Input name file or path to upload:")
+    send_data_to_upload(name_file,client)
+def send_data_to_upload_not_bar(name_file):
+    client = init()
+    mode = "upload" 
+    client.send(mode.encode())
+    file = open(name_file,"rb")
+    send_header_to_server(client, name_file,file)
+    while True:
+        chunk = file.read(BUFFER)  
+        if not chunk:
+            break  
+        client.send(chunk)
+    file.close()
+    print(name_file + " successful download.")
 
-def process_name_file(path_file):
-    processed_name_file = path_file
-    i = 1
-    while os.path.exists(processed_name_file):
-        name, extension = os.path.splitext(path_file) 
-        processed_name_file = f"{name}({i}){extension}"
-        i += 1
-    print('Dia chi file o server', processed_name_file)
-    return processed_name_file
 
-def get_content(connection, name_file_processed, file_size):
-    #Lấy nội dung file
-    with open(name_file_processed, 'ab') as f:
-        received_data = 0
-        while received_data < file_size:
-            data = connection.recv(BUFFER)
-            if not data:
-                if received_data < file_size:
-                    connection.send(message_notenough.ljust(LENGTH_MESS,' ').encode())
-                    os.remove(name_file_processed)
-                    # connection.close()
-                print('Het data')
-                break
-            received_data += len(data)
-            f.write(data)
-        if received_data == file_size: 
-            connection.send(message_enough.ljust(LENGTH_MESS,' ').encode())
+def upload_multithreaded():
+    name_folder = input("Input name path folder:")
+    list_file = os.listdir(name_folder)
+    threads = []
+    for file in list_file:
+        name_file_path = name_folder + "\\" + file
+        thread = threading.Thread(target = send_data_to_upload_not_bar,args = (name_file_path,))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
 
-def download_from_server(client, path_name):
-    client.send(path_name.ljust(BUFFER).encode())
-    received_message = client.recv(LENGTH_MESS).decode().strip()
-    print(received_message)
-    if(received_message == message_success):
-        processed_file_name = process_name_file(path_name)
-        file_size = int(client.recv(LENGTH_SIZE).decode().strip())
-        get_content(client, processed_file_name, file_size)
+
+
+def upload_orderly(client):
+    name_folder = input("Input name path folder:")
+    list_file = os.listdir(name_folder)
+    number_of_file = str(len(list_file))
+    client.send(number_of_file.ljust(LENGTH_NUMBER_OF_FILE,' ').encode(ENCODING))
+    for name_file in list_file:
+        print("Upload file " + name_file)
+        name_file_path = name_folder + "\\" + name_file
+        send_data_to_upload(name_file_path,client)
+
+
+
+def find_path_to_save_file(name_path,name_file):
+    name_path_and_file = name_path
+    if name_path_and_file[len(name_path_and_file)-1] == '/':
+        name_path_and_file += name_file
     else:
-        print('file khong ton tai tren server')
+        name_path_and_file =name_path_and_file+ "/" +name_file
 
+    if os.path.exists(name_path_and_file) == True:
+        os.remove(name_path_and_file)
+
+    file = open(name_path_and_file,"x")
+    file.close()
+    return name_path_and_file
+
+def get_content(client,name_file_and_path,file_size):
+    received_size = 0
+    with open(name_file_and_path,"ab") as file:
+        number_of_repeat= math.ceil(file_size/ BUFFER)
+        with alive_bar(number_of_repeat, title="Downloading") as bar:
+            for _ in range(number_of_repeat):
+                data = client.recv(BUFFER)
+                bytes_to_write = min(len(data), file_size - received_size)
+                file.write(data[:bytes_to_write])
+                received_size += bytes_to_write
+                time.sleep( ) 
+                bar()
+                if not data:
+                    if received_size < number_of_repeat:
+                        client.send(message_notenough.ljust(LENGTH_MESS,' ').encode(ENCODING))
+                        os.remove(name_file_and_path)
+                        print("Error: The data is insufficient")
+                        break
+                if received_size == file_size:
+                    break
+        if received_size == number_of_repeat: 
+                client.send(message_enough.ljust(LENGTH_MESS,' ').encode(ENCODING))
+
+def download(client):
+    name_file = input("Input name file to download:")
+    name_path = input("Input path file to save: ")
+    name_path_file = find_path_to_save_file(name_path,name_file)
+    client.send(name_file.ljust(BUFFER,' ').encode(ENCODING))
+    message = client.recv(LENGTH_MESS).decode().strip()
+    if message == message_success:
+        file_size = int(client.recv(LENGTH_SIZE).decode().strip())
+        get_content(client,name_path_file,file_size)
+        client.close()
+    else:
+        print("The file doesn't exit in the server")
+        client.close()
+def choose_mode():
+    print(" ")
 
 def main():
-    client = init_client()
-    mode =input('Input mode: ') # chưa có download
-
-    if mode == 'upload':
-        name_file = input("Input name file to upload:")
-        client.send(mode.encode())
-        file_size = send_path_and_size(client, name_file)
-        send_content_to_server(client,name_file,file_size)
-
-    elif mode == 'download':
-        path_file = input("Input name file to download: ")
-        client.send(mode.encode())
-        download_from_server(client, path_file)
-    else:
-        print('Sai mode')
-
-
-    
+    client = init()
+    #gửi mode
+    #upload
+    mode = "upload" 
+    client.send(mode.ljust(LENGTH_MODE).encode())
+    upload(client)
+    #download
+    # mode = "download" 
+    # client.send(mode.ljust(LENGTH_MODE).encode(ENCODING))
+    # download(client) 
+    # upload_orderly(client)
+    #upload_orderly(client)
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,23 +1,39 @@
 import socket
 import os
 import threading
+import logging
+import time
 
 ENCODING = 'utf-8'
-PORT_SERVER = 5051
+PORT_SERVER = 9999
 HOST_SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS_SERVER = (HOST_SERVER, PORT_SERVER)
 LENGTH_SIZE = 16 #16 bytes để truyền kích thước file
-LENGTH_MODE = 8  # 8 bytes để đọc mode
-LENGTH_MESS = 13 # 13 bytes tín hiệu phản hồi lại bên gửi
+LENGTH_MODE = 16 # 8 bytes để đọc mode
+LENGTH_MESS = 16 # 16 bytes tín hiệu phản hồi lại bên gửi
+LENGTH_NUMBER_OF_FILE = 10
 BUFFER = 1024   # bộ nhớ đệm 1024 bytes
 message_notenough = 'NOTENOUGH'
 message_enough = 'ENOUGH'
 message_success = 'SUCCESS'
+message_failure = 'FAILURE'
 message_error_notfound = 'ERRORNOTFOUND'
+message_setup_first_pass_word = 'SETUP_pass_word'
+message_setup_first_pin = 'SETUP_PIN'
 data_server_folder = "C:/database"
 
-#Init
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Cấu hình logging
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(data_server_folder+'/'+'server_log1.txt')
+    ]
+)
+
 
 #Init server
 def init_server():
@@ -28,18 +44,272 @@ def init_server():
 
 #Listening
 def listening(server_socket):
-    print(f'Server is lisening: HOST {HOST_SERVER}')
-    while True:
-        conn, adrr = server_socket.accept()
-        print(f'{adrr} connected')
-        mode = conn.recv(LENGTH_MODE).decode().strip()
-        if mode == 'upload':
-            client_thread = threading.Thread(target= response_upload, args= (conn, ))
-            client_thread.start()
-        elif mode == 'download':
-            client_thread = threading.Thread(target= response_download, args= (conn, ))
-            client_thread.start()
+    try:
+       listening_support(server_socket)
+    except KeyboardInterrupt:
+        print("Server is shutting down...")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        server_socket.close()
+        logging.warning("Socket closed. Server stopped.")    
 
+
+#Xử lí kết nối với client
+def handle_client_connection(conn, addr):
+    try:
+        # process_login_client(conn)
+        logging.info(f'{addr} connected')
+
+        # Nhận mode (upload hoặc download)
+        mode = conn.recv(LENGTH_MODE).decode().strip()
+
+        # Xử lý mode
+        if mode == 'upload':
+            response_upload(conn)  # Xử lý upload
+        elif mode == 'download':
+            response_download(conn)  # Xử lý download
+        else:
+            print('mode is not exist')
+            conn.close()
+
+    except Exception as e:
+        print(f"Error handling client {addr}: {e}")
+    finally:
+        conn.close()  # Đảm bảo kết nối được đóng sau khi hoàn tất xử lý
+
+
+def response_upload(connection):
+    try:
+        process_login_upload(connection)
+        name_file_processed = get_name_file_processed(connection)
+        #Nhan size
+        try:
+            file_size = int(connection.recv(LENGTH_SIZE).decode().strip())
+            print(file_size)
+        except ValueError as e:
+            print(f"Invalid file size received: {e}")
+            return
+         # Nhận và lưu nội dung file
+        try:
+            logging.info(f"upload {name_file_processed} on server")
+            get_content(connection, name_file_processed, file_size)
+        except (OSError, IOError) as e:
+            print(f"File I/O error: {e}")
+            return
+    except socket.error as e:
+        print(f"Socket error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        logging.info('yeu cau upload da dong')
+        connection.close()
+
+def response_download(connection):
+    #Nhan path file
+    try:
+        new_path = get_name_file_processed
+        response_download_support(connection, new_path)
+    except ConnectionResetError:
+        print("Client disconnected unexpectedly.")
+    except socket.error as e:
+        print(f"Socket error during download: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        print("Closing connection.")
+        connection.close()
+
+
+#client
+def reponse_upload_orderly(connection):
+    number_of_file = connection.recv(LENGTH_NUMBER_OF_FILE).decode().strip()
+    number_of_file = int(number_of_file)
+    for step in range(number_of_file):
+        response_upload(connection)
+'''
+CÁC HÀM HỖ TRỢ
+||||||||||||||
+""""""""""""""
+'''
+# xử lí download chưa có ngoại lệ 
+def response_download_support(connection, new_path):
+        if os.path.exists(new_path):
+            # gui tin hieu thanh cong
+            connection.send(message_success.ljust(LENGTH_MESS).encode(ENCODING))
+            print('Tìm thấy file trên server')
+            file_size = os.path.getsize(new_path) #lấy kích thước
+            #gửi tên và kích thước
+            connection.send(str(file_size).ljust(LENGTH_SIZE).encode(ENCODING))
+            with open(new_path, 'rb') as f:
+                while True:
+                    data = f.read(BUFFER)
+                    if not data: break
+                    connection.send(data)
+        else:
+            connection.send(message_error_notfound.ljust(LENGTH_MESS).encode(ENCODING))
+            print('file không tồn tại')
+        mess_from_client = connection.recv(LENGTH_MESS).decode().strip()
+
+        # nếu gửi lại mãi mà vẫn không được thì sao ???? 
+        if mess_from_client == message_notenough:
+            print('Gửi đi thất bại')
+
+
+def set_pass_word_for_first_time(conn):
+    print('do ne')
+    pass_word_path = data_server_folder + '/' + str(conn.getpeername()[0]) + '/' + '_pass_word.txt'
+    conn.send(message_setup_first_pass_word.ljust(LENGTH_MESS).encode(ENCODING))
+    _pass_word = conn.recv(LENGTH_SIZE).decode().strip()
+    print(_pass_word)
+    with open(pass_word_path,'w') as file:
+        file.write(_pass_word)
+    return(_pass_word)
+
+def get_pass_word(conn):
+    pass_word_path = get_path_of_server("_pass_word.txt",conn.getpeername()[0])
+    print(pass_word_path)
+    try:
+        if not os.path.exists(pass_word_path):
+            fd = os.open(pass_word_path, os.O_CREAT | os.O_WRONLY)
+            os.close(fd)
+        if os.path.getsize(pass_word_path) == 0:
+            return(set_pass_word_for_first_time(conn))
+        else: 
+            conn.send(message_success.ljust(LENGTH_MESS).encode(ENCODING))
+
+                # Đọc nội dung file
+            with open(pass_word_path, "r") as file:
+                content = file.read().strip()  # Xóa khoảng trắng hoặc xuống dòng    
+            # Kiểm tra nội dung có phải số nguyên
+            if content.isdigit():
+                number = int(content)
+                print(f"Số nguyên trong file: {number}")
+                return (number)
+            else:
+                print("Dữ liệu trong file không phải số nguyên.")
+    except Exception as e:
+        print(f"Đã xảy ra lỗi: {e}")
+
+def validate_client(conn):
+    try:
+        addr = conn.getpeername()[0]
+        valid_key = get_pass_word(conn)  # Khóa bí mật
+        # conn.settimeout(10000)
+        valid_key = int(valid_key)
+        key = conn.recv(LENGTH_SIZE).decode().strip()
+        key = int(key)
+        if key != valid_key:
+            print(f"Client at {addr} provided invalid key: {key}")
+            return False
+        return True
+    except Exception as e:
+        print(f"Validation failed for {addr}: {e}")
+        return False
+
+def set_pin_for_first_time(conn):
+    print('do ne')
+    pin_path = data_server_folder + '/' + str(conn.getpeername()[0]) + '/' + '_pin.txt'
+    conn.send(message_setup_first_pin.ljust(LENGTH_MESS).encode(ENCODING))
+    _pin = conn.recv(LENGTH_SIZE).decode().strip()
+    print(_pin)
+    with open(pin_path,'w') as file:
+        file.write(_pin)
+    return(_pin)
+
+def get_pin(conn):
+    pin_path = get_path_of_server("_pin.txt",conn.getpeername()[0])
+    print(pin_path)
+    try:
+        if not os.path.exists(pin_path):
+            fd = os.open(pin_path, os.O_CREAT | os.O_WRONLY)
+            os.close(fd)
+        if os.path.getsize(pin_path) == 0:
+            return(set_pin_for_first_time(conn))
+        else: 
+            conn.send(message_success.ljust(LENGTH_MESS).encode(ENCODING))
+
+                # Đọc nội dung file
+            with open(pin_path, "r") as file:
+                content = file.read().strip()  # Xóa khoảng trắng hoặc xuống dòng    
+            # Kiểm tra nội dung có phải số nguyên
+            if content.isdigit():
+                number = int(content)
+                print(f"Số nguyên trong file: {number}")
+                return (number)
+            else:
+                print("Dữ liệu trong file không phải số nguyên.")
+    except Exception as e:
+        print(f"Đã xảy ra lỗi: {e}")    
+
+def validate_client_when_upload(conn):
+    try:
+        addr = conn.getpeername()[0]
+        valid_key = get_pin(conn)  # Khóa bí mật
+        print('khoa', valid_key)
+        # conn.settimeout(10000)
+        valid_key = int(valid_key)
+        key = conn.recv(LENGTH_SIZE).decode().strip()
+        key = int(key)
+        print('key', key)
+        if key != valid_key:
+            print(f"Client at {addr} provided invalid key: {key}")
+            return False
+        return True
+        print('giong pin')
+    except Exception as e:
+        print(f"Validation failed for {addr}: {e}")
+        return False
+#Listening
+def listening_support(server_socket):
+        logging.info("Server {HOST_SERVER} started and is listening for connections:...")
+        while True:
+            try:
+                conn, addr = server_socket.accept()
+                client_thread = threading.Thread(target=handle_client_connection, args=(conn, addr))
+                client_thread.start()
+            except ConnectionResetError as e:
+                print(f"Connection reset by client: {e}")
+            except socket.error as e:
+                print(f"Socket error: {e}")
+            except Exception as e:
+                print(f"Unexpected errore: {e}")
+#xử lí thao tác nhập mã mat khau để dang nhap
+def process_login_client(conn):
+        addr = conn.getpeername()[0]
+        is_validated_client =  validate_client( conn)
+        while not is_validated_client:
+            print(f"Connection from {addr} rejected due to failed validation.")
+            conn.send(message_failure.ljust(LENGTH_MESS).encode())
+            is_validated_client = validate_client(conn)
+            # conn.close()
+            # return
+
+        conn.send(message_success.ljust(LENGTH_MESS).encode())
+
+# hàm trả về địa chỉ của file trên server (đã thêm cơ số phía sau)
+def get_name_file_processed(connection):
+    #Nhan name file
+        name_file = connection.recv(BUFFER).decode().strip()
+        print(name_file)
+        new_path = get_path_of_server(name_file,connection.getpeername()[0])
+        #Xu li name file
+        name_file_processed = process_name_file(new_path)
+        return(name_file_processed)
+
+#xử lí thao tác nhập mã pin khi upload
+def process_login_upload(conn):
+        addr = conn.getpeername()[0]
+        is_validated_client =  validate_client_when_upload(conn)
+        while not is_validated_client:
+            print(f"Connection from {addr} rejected due to failed validation.")
+            conn.send(message_failure.ljust(LENGTH_MESS).encode())
+            is_validated_client = validate_client_when_upload(conn)
+            # conn.close()
+            # return
+        conn.send(message_success.ljust(LENGTH_MESS).encode())
+
+        
 # tránh trùng tên
 def process_name_file(path_file):
     processed_name_file = path_file
@@ -51,18 +321,14 @@ def process_name_file(path_file):
     print('Dia chi file o server', processed_name_file)
     return processed_name_file
 
-
-
-# tạo vị trí mới khi nhận file 
-def create_new_path_for_server(src,client_ip):
+# tạo vị trí mới khi nhận file (chưa tinh chỉnh để không bị trùng)
+def get_path_of_server(src,client_ip):
     file_name = os.path.basename(src)
     os.makedirs(data_server_folder + '/' + str(client_ip),exist_ok=True)
     new_path = data_server_folder + '/' + str(client_ip) + '/' + file_name
-    print('Đường truyền mới', new_path)
     return new_path
 
-
-
+#nhận nội dung
 def get_content(connection, name_file_processed, file_size):
     #Lấy nội dung file
     with open(name_file_processed, 'ab') as f:
@@ -73,56 +339,13 @@ def get_content(connection, name_file_processed, file_size):
                 if received_data < file_size:
                     connection.send(message_notenough.ljust(LENGTH_MESS,' ').encode(ENCODING))
                     os.remove(name_file_processed)
-                    # connection.close()
-                print('Het data')
                 break
             received_data += len(data)
             f.write(data)
-        if received_data == file_size: 
-            connection.send(message_enough.ljust(LENGTH_MESS,' ').encode(ENCODING))
+        connection.send(message_enough.ljust(LENGTH_MESS,' ').encode(ENCODING))
 
 
-def response_upload(connection):
-    #Nhan name file
-    path_file = connection.recv(BUFFER).decode().strip()
-    print(path_file)
-    new_path = create_new_path_for_server(path_file,connection.getpeername()[0])
-    #Xu li name file
-    name_file_processed = process_name_file(new_path)
 
-    #Nhan size
-    file_size = int(connection.recv(LENGTH_SIZE).decode().strip())
-    print(file_size)
-    get_content(connection, name_file_processed, file_size)
-    connection.close()
-
-def response_download(connection):
-    #Nhan path file
-    path_file = connection.recv(BUFFER).decode().strip()
-    print(path_file)
-    new_path =  create_new_path_for_server(path_file,connection.getpeername()[0])
-    print(new_path)
-    if os.path.exists(new_path):
-        # gui tin hieu thanh cong
-        connection.send(message_success.ljust(LENGTH_MESS).encode(ENCODING))
-
-        file_size = os.path.getsize(new_path) #lấy kích thước
-        #gửi tên và kích thước
-        connection.send(str(file_size).ljust(LENGTH_SIZE).encode(ENCODING))
-        while True:
-            with open(new_path, 'rb') as f:
-                data = f.read(BUFFER)
-                if not data:
-                    break
-                connection.send(data)
-    else:
-        connection.send(message_error_notfound.ljust(LENGTH_MESS).encode(ENCODING))
-        
-    mess_from_client = connection.recv(LENGTH_MESS).decode().strip()
-
-    # nếu gửi lại mãi mà vẫn không được thì sao ???? 
-    if mess_from_client == message_notenough:
-        response_download(connection)
 
 def main():
     server_socket = init_server()
